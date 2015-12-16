dc5e02a7b9212f0ee651a0ea0dcd058cf5c14662
Rita Lee <rita_lee@hiiir.com>
2015-12-16 20:27:57 +0800

    rita-1

--- practices/practice_1_js_basic/basic_practice.js
+++ practices/practice_1_js_basic/basic_practice.js
@@ -6,10 +6,10 @@ var Basket = function (n)  {
 
 // 使用 Basket 建構式建立一個 apple 的變數，建構式參數填入 numbers
 // 把問號換成可以執行的程式
-var apple = ?
+var apple = new Basket(numbers);
 
 // 直接在下行註解內將問號改成正確答案
-// apple.appleNum = ?
+// apple.appleNum = 100; 
 
 var num = apple;
 
@@ -17,4 +17,4 @@ apple.appleNum = 20;
 num.appleNum = 10;
 
 // 直接在下行註解內將問號改成正確答案
-// apple.appleNum = ?
+// apple.appleNum = 10
